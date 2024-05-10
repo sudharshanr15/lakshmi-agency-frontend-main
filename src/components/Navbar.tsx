beaed15ec49dcd1cec9173531cd15a85f4c69fbf
Sudharshan <sudharshanrms2004@gmail.com>
2024-05-10 17:34:16 +0530

    Responsive top navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,8 @@ import PersonIcon from '@mui/icons-material/Person';
 import MyLocationIcon from '@mui/icons-material/MyLocation';
 import CategoryIcon from '@mui/icons-material/Category';
 import LocalMallIcon from '@mui/icons-material/LocalMall';
+import MenuIcon from '@mui/icons-material/Menu';
+import ContactSupportIcon from '@mui/icons-material/ContactSupport';
 import Link from "next/link";
 import CategoryMenu from "./CategoryMenu";
 
@@ -36,6 +38,28 @@ function Navbar() {
                         <PersonIcon fontSize="large" />
                         <span className="inline-block ms-2">Jhon Doe</span>
                     </button>
+                </div>
+            </div>
+            <div className="xl:hidden w-full p-4">
+                <div className="flex justify-between mb-4">
+                    <button>
+                        <MenuIcon className="text-secondary-yellow" />
+                    </button>
+                    <Link href={"/"} className="text-white font-bold">
+                        <img src="/assets/images/Lakshmi.png" className="inline h-full max-h-[24px] w-auto" alt="Brand Image" />
+                        <span className="ms-2 text-sm text-nowrap">LAKSHMI AGENCY</span>
+                    </Link>
+                    <button>
+                        <ContactSupportIcon className="text-secondary-yellow" />
+                    </button>
+                </div>
+                <div className="bg-white text-black rounded-md py-2 p-2 text-sm flex">
+                    <input
+                        type="text"
+                        className="flex-grow focus:outline-none placeholder-gray-400 me-4"
+                        placeholder="Search for any products..."
+                    />
+                    <SearchIcon className="text-secondary-yellow" />
                 </div>
             </div>
             <hr className="hidden xl:block border-b border-[#E9EBF0] my-3" />
