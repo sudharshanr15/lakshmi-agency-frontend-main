fb0e60306bfb0796abb21ab82fdb4ddd832be159
Sudharshan <sudharshanrms2004@gmail.com>
2024-05-14 20:08:08 +0530

    Added footer component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,7 +8,7 @@ function Dashboard() {
   return (
     <>
         <MobileNavbar />
-        {/* <Footer /> */}
+        <Footer />
     </>
     )
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,72 +6,40 @@ import Link from 'next/link';
 
 export function Footer() {
     return (
-        <>
-            <div className="bg-[#F8F9FA] z-[100] sm:block hidden">
-                <div className="mx-10 mt-12  lg:mt-16 mb-5">
-                    <div className="grid grid-cols-2  ">
-                        <div className="mx-2">
-                            <div className="flex items-center p-5">
-                                <img
-                                    src={"/assets/images/Lakshmi.png"}
-                                    alt="Logo"
-                                    className="w-40 h-20"
-                                />
-                            </div>
-                            <p className="">
-                                The objective of Lakshmi Agency is to establish
-                                a marketplace for selling interior construction
-                                and decoration products for both living and
-                                working spaces. The aim is to provide customers
-                                with a one-stop-shop where they can find all the
-                                necessary materials and benefit from exclusive
-                                offers . This will ultimately save them time,
-                                money, and energy in their search for interior
-                                design solutions.
-                            </p>
+        <footer className="bg-[#F8F9FA] hidden xl:block">
+            <div className="flex max-w-[1800px] mx-auto px-4 py-16">
+                <div className='max-w-[430px] text-sm'>
+                    <div className='flex gap-1 items-center'>
+                        <img src="/assets/images/Lakshmi.png" alt="" className='max-w-[50px]' />
+                        <span className='uppercase text-sm-bold'>LAKSHMI AGENCY</span>
+                    </div>
+                    <p className='mt-4'>
+                    The objective of Lakshmi Agency is to establish a marketplace for selling interior construction and decoration products for both living and working spaces. The aim is to provide customers with a one-stop-shop where they can find all the necessary materials and benefit from exclusive offers . This will ultimately save them time, money, and energy in their search for interior design solutions.
+                    </p>
+                </div>
+                <div className="flex-grow md:ms-20 xl:ms-40">
+                    <div className="flex md:gap-x-20 xl:gap-x-40">
+                        <div className='flex flex-col gap-4'>
+                            <h5 className='text-primary capitalize text-body-bold'>About Us</h5>
+                            <Link href={""} className='text-sm font-medium capitalize'>Our Stories</Link>
+                            <Link href={""} className='text-sm font-medium capitalize'>Our Careers</Link>
+                            <Link href={""} className='text-sm font-medium capitalize'>Our Partners</Link>
                         </div>
-
-                        <div className="mt-2 md:mt-20 mx-2 p-4 flex justify-evenly">
-                            {/*  */}
-                            {/* <div>
-                  <h1 className="text-2xl font-bold mt-5 text-[#004b71]">
-                    About us
-                  </h1>
-                  <p className="mt-4 mx-auto"></p>
-                  <span className="text-1xl">
-                    <p className="mt-3">Our careers</p>
-                    <p className="mt-3">Our partners</p>
-                    <p className="mt-3">Our clients</p>
-                  </span>
-                </div> */}
-                            {/*  */}
-                            <div>
-                                <h1 className="text-2xl text-[#004b71] font-bold mt-5">
-                                    Resources
-                                </h1>
-                                <p className="mt-4 mx-auto"></p>
-                                <span className=" text-1xl">
-                                    <p className="mt-3">Blogs</p>
-                                    {/* <p className="mt-3">Offer page</p>
-                    <p className="mt-3">Tutorials</p>
-                    <p className="mt-3">Clients page</p> */}
-                                </span>
-                            </div>
-                            {/*  */}
-                            <div>
-                                <h1 className="text-2xl font-bold mt-5 text-[#004b71]">
-                                    Reach us
-                                </h1>
-                                <p className="mt-4 mx-auto"></p>
-                                <span className=" text-1xl">
-                                    <p className="mt-3">Contact us</p>
-                                </span>
-                            </div>
+                        <div className='flex flex-col gap-4'>
+                            <h5 className='text-primary capitalize text-body-bold'>Resources</h5>
+                            <Link href={""} className='text-sm font-medium capitalize'>Blogs</Link>
+                            <Link href={""} className='text-sm font-medium capitalize'>Offers Page</Link>
+                            <Link href={""} className='text-sm font-medium capitalize'>FAQ</Link>
+                        </div>
+                        <div className='flex flex-col gap-4'>
+                            <h5 className='text-primary capitalize text-body-bold'>Reach Us</h5>
+                            <Link href={""} className='text-sm font-medium capitalize'>Contact Us</Link>
                         </div>
                     </div>
                 </div>
-
-                <footer className="hidden md:block shadow bg-[#0A4E71]">
+            </div>
+            <div className="md:block shadow bg-primary">
+                <div className="max-w-[1800px] mx-auto">
                     <div className="w-full p-4 md:flex md:items-center md:justify-between">
                         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-white  sm:mt-0">
                             <li>
@@ -79,7 +47,7 @@ export function Footer() {
                                     href="#"
                                     className="mr-4 hover:underline md:mr-6"
                                 >
-                                    Copyrights@2022.All rights reserved by
+                                    Copyrights@2022 All rights reserved by
                                     Lakshmi Agency
                                 </Link>
                             </li>
@@ -142,9 +110,8 @@ export function Footer() {
                             </span>
                         </span>
                     </div>
-                </footer>
+                </div>
             </div>
-            <div className="h-24 sm:hidden block"></div>
-        </>
+        </footer>
     );
 }
