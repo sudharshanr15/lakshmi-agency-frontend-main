2bdb54bede7373720ca2ddad8412b7cf4c673b07
Sudharshan <sudharshanrms2004@gmail.com>
2024-05-25 20:50:18 +0530

    Added loading files for profile

---
+++ src/components/PageLoading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Loading from './Loading'
+
+function PageLoading() {
+  return (
+    <div className="flex items-center justify-center w-full">
+        <Loading />
+    </div>
+  )
+}
+
+export default PageLoading
